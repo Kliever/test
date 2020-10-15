53f68f1882f1ac50be09caad614987a1482ce453
Kliever <64734174+Kliever@users.noreply.github.com>
2020-10-15 18:57:42 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$(function(){
+
+
+// Слайдер
+  $('.apps__slider').slick({
+
+    prevArrow: '<button class="slick-btn slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button class="slick-btn slick-next"><img src="images/arrow-right.svg" alt=""></button>'
+
+  });
+
+
+  //Плавно выезжание - заезжание
+  // $('.').on('click', function () {
+  //   $('.').slideToggle();
+  // });
+
+
+
+  
+
+
+//Добавление класса при клике на элемент
+  // $('.').on('click', function () {
+  //   $('.').toggleClass('');
+  // })
+
+
+
+
+
+// загрузка элементов при скролле
+// new WOW().init();      
+});
